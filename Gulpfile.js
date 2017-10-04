1c9f80f3fbe88186ebad56e3218b5319e4cffd5c
dixmacmini04 <dixmacmini04@Mac-mini-de-Mirelis.local>
2017-10-04 17:38:31 -0400

    Initial commit

---
+++ Gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var sourcemaps = require('gulp-sourcemaps');
+var reload = browserSync.reload;
+
+gulp.task('sass', function() {
+	return gulp.src('./src/css/main.scss')
+		.pipe(sourcemaps.init())
+		.pipe(sass().on('error', sass.logError))
+		.pipe(sourcemaps.write('./'))
+		.pipe(gulp.dest('./src/css/'))
+		.pipe(reload({ stream: true }));
+});
+
+gulp.task('serve',['sass'], function() {
+	browserSync({
+		server: {
+			baseDir: 'src'
+		}
+	});
+
+	gulp.watch('src/css/*.scss', ['sass']).on('change', reload);
+	gulp.watch('src/*.html').on('change', reload);
+});
---
+++ README.md
@@ -0,0 +1,22 @@
+# real-como-santal
+Demo de la maquetaci&oacute;n de los layouts de la app Real como Santal
+
+## Instalaci&oacute;n
+Instalar dependencias del proyecto
+``` zsh
+    npm install
+```
+
+Para ver un preview usando ``` browser-sync ```:
+``` zsh
+    gulp serve
+```
+
+## Uso
+Para pasar al pr&oacute;ximo layout solo basta hacer click en cualquier espacio dentro del mismo.
+
+En los layouts donde habrán formularios y/o botones, hacer click en los botones para pasar al pr&oacute;ximo layout.
+
+## Ayuda
+Referirse a Fernando Gruning [[fernando@dix.do](mailto:fernando@dix.do)].
+
